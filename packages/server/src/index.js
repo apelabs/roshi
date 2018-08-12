f8275f2adb0722fb55456c8b85410c28d78e432a
Xavier <xavier@xmbp.local>
2018-08-12 23:51:17 +0100

    add cra webclient

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -2,8 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const app = express();
+
+app.use('/', (req, res) => {
+  res.statusCode = 200;
+	res.json({ status:"success", message:"Hello world!", data:{ } })
+})
 
 app.listen(port, () => {
   console.log(`Server listing on ${port}`);
