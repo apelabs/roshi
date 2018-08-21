09ca9ea46965832b56ebdca653a240ce0f582d8d
Xavier Fuentes <xavier@xmbp.local>
2018-08-21 10:55:52 +0100

    change node to 10.9 to allow python by default (required for bcrypt)

--- packages/server/src/user/models/user.js
+++ packages/server/src/user/models/user.js
@@ -1,25 +1,40 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-module.exports = mongoose.model(
-  'User',
-  new mongoose.Schema(
-    {
-      firstName: {
-        type: String,
-        required: true,
-      },
-      lastName: String,
-      email: {
-        type: String,
-        required: true,
-        unique: true,
-      },
-      password: {
-        type: String,
-        required: true,
-      },
-      avatarUrl: String,
+const SALT_WORK_FACTOR = 10;
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
-  )
+    lastName: String,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatarUrl: String,
+  },
+  { timestamps: true }
 );
+
+UserSchema.pre('save', async function preSave(next) {
+  var user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+  const hash = await bcrypt.hash(user.password, salt);
+
+  user.password = hash;
+  next();
+});
+
+module.exports = mongoose.model('User', UserSchema);
