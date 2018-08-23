9a5bc4f3929bfa3800b3260c83787a67b94ac70e
byverdu <byverdu@gmail.com>
2018-08-24 00:52:28 +0100

    wip adding react-styleguidist functionality

--- packages/ui/src/__stories__/Image/Image.story.js
+++ packages/ui/src/__stories__/Image/Image.story.js
@@ -4,6 +4,6 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import Image from '../Image';
+import Image from '../../components/Image';
 
 storiesOf('Image', module).add('with src', () => <Image src="http://any" />);
