23aaca39dee7c10e44f5706014175bbcad2e52b9
byverdu <byverdu@gmail.com>
2018-08-25 10:09:59 +0100

    updated stories for Image component

--- packages/ui/src/__stories__/Image/Image.story.js
+++ packages/ui/src/__stories__/Image/Image.story.js
@@ -6,4 +6,9 @@ import { linkTo } from '@storybook/addon-links';
 
 import Image from '../../components/Image';
 
-storiesOf('Image', module).add('with src', () => <Image src="http://any" />);
+storiesOf('Image', module)
+  .add('without src attribute', () => <Image />)
+  .add('with width', () => <Image width={200} />)
+  .add('with withshadow', () => <Image withshadow />)
+  .add('with withshadow withradius', () => <Image withshadow withradius="50%" />)
+  .add('with extra styles', () => <Image style={{ transform: 'rotate(45deg)' }} />);
