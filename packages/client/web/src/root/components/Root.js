4013df390db7c6466ad8fa1e1d8a1ba0d1731623
byverdu <byverdu@gmail.com>
2018-08-29 00:54:58 +0100

    wip adding ui library to other packages

--- packages/client/web/src/root/components/Root.js
+++ packages/client/web/src/root/components/Root.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './Root.css';
 
+import { Image } from '@apelabs/roshi-ui';
+import { Button } from '@apelabs/roshi-ui';
+
 class App extends Component {
   render() {
     return (
@@ -9,6 +12,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <Image />
+          <Button children="logo.svg" />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
